a4d3fbd12afefdd45acadd351f154ff621e31a48
satyaswarup-banerjee-kilowott <92801510+satyaswarup-banerjee-kilowott@users.noreply.github.com>
2023-04-18 15:15:28 +0530

    commit

--- next.config.js
+++ next.config.js
@@ -1,11 +1,10 @@
 module.exports = {
   reactStrictMode: true,
-  eslint: { ignoreDuringBuilds: true },
-  experimental: {
-        images: {
-            unoptimized: true
-        }
-   },
+  // experimental: {
+  //       images: {
+  //           unoptimized: true
+  //       }
+  //  },
   publicRuntimeConfig: {
     BACKEND_URL: process.env.NEXT_PUBLIC_BACKEND_URL || "envnotset",
     GOOGLE_CLIENT_ID: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID || "envnotset",
@@ -13,15 +12,7 @@ module.exports = {
     WEBFLOW_URL:
       process.env.NEXT_PUBLIC_WEBFLOW_URL ||
       "https://landing-page-ac72bc.webflow.io",
-  },//Added img protocols
-  // images: {
-  //   remotePatterns: [
-  //     {
-  //       protocol: "https",
-  //       hostname: "**",
-  //     },
-  //   ],
-  // },
+  },
   images: {
     domains: ['main.d21h2ko0ro2f7y.amplifyapp.com'],
   },
